71bcd98efb8e007bbfe381559a2788d14dd1d014
corenting <corenting@gmail.com>
2016-03-11 00:17:42 +0000

    Project start

---
+++ MSI Keyboard Manager/Constants.cs
@@ -0,0 +1,42 @@
+﻿namespace MSI_Keyboard_Manager
+{
+    class Constants
+    {
+        public enum Colors
+        {
+            Off,
+            Red,
+            Orange,
+            Yellow,
+            Green,
+            Sky,
+            Blue,
+            Purple,
+            White
+        };
+
+        public enum Levels
+        {
+            High,
+            Med,
+            Low,
+            Light
+        }
+
+        public enum Regions
+        {
+            Left = 1,
+            Middle,
+            Right
+        }
+
+        public enum Modes
+        {
+            Normal = 1,
+            Gaming,
+            Breathe,
+            Audio,
+            Wave
+        }
+    }
+}
---
+++ MSI Keyboard Manager/Form1.cs
@@ -0,0 +1,31 @@
+﻿using HidSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MSI_Keyboard_Manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var loader = new HidDeviceLoader();
+            var device = loader.GetDevices(0x1770, 0xff00).First();
+            HidStream stream;
+            device.TryOpen(out stream);
+            byte[] array = {1, 2, 66, 1, (int) Constants.Colors.Red, 3, 0, 236};
+            stream.SetFeature(array);
+        }
+    }
+}
